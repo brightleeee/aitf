b1cbca55854ed1852c08d3eb36df360ca7d3a996
brightleeee <84517690+brightleeee@users.noreply.github.com>
2021-06-03 16:21:28 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,59 +1,51 @@
-import pandas as pd
-from flask import Flask, render_template, request
-import main
+from typing import Any
 
+from flask import Flask, request, session,g,redirect,url_for, abort, flash, render_template, jsonify, json
+import pandas as pd
+import sqlite3
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
+def yeon():
+    return render_template("login_test.html")
+@app.route('/index.html')
+def yeon1():
+    return render_template("index.html")
+@app.route('/password.html')
+def yeon2():
+    return render_template("password.html")
+@app.route('/test.html')
+def yeon3():
+    return render_template("test.html")
 
-    return render_template('index.html')
+@app.route('/test3.html')
+def yeon4():
+    return render_template("test3.html")
 
-@app.route('/form')
-def student():
-    # ROOT URL 로 접근했을 때 8_sending_form_data.html 을 렌더링한다.
-    #  return render_template('sending_form_data.html')
+@app.route('/login.html')
+def yeon5():
+    return render_template("login.html")
 
-    return render_template('sending_form_data.html')
+@app.route('/test', methods=['POST'])
+def languages():
+    value1 = request.form['itemid']
+    value2 = "경기도"
+    print(value1)
+    print(value2)
+    # value1 = "상가"
+    print("데이터 넘어왔나?")
 
-@app.route('/result', methods = ['POST', 'GET'])
-def result():
-    # result URL 로 접근했을 때, form 데이터는 request 객체를 이용해 전달받고
-    # 이를 다시 8_sending_form_data_result.html 렌더링하면서 넘겨준다.
-    if request.method == 'POST':
-        result = request.form
-        result2 = dict(request.form)
-        result3 = pd.DataFrame(result, index=[0])
-        result4 = result3.to_html()
+    conn = sqlite3.connect('trest.db')
+    curs = conn.cursor()
 
-        print(result3)
-        print(result3.values)
-        print(result3)
+    curs.execute('SELECT * FROM test1 where 용도=?, AND 주소지=?' (value1, value2, )) #test1에 있는 값을 조건 없이 불러옴
 
+    # row_headers = [x[0] for x in curs.description]
+    rows = curs.fetchall()
+    print(rows)
+    print("test2")
+    return jsonify(rows)
 
-        return render_template("sending_form_data_result2.html", tables = result3, titles = '2')
-
-    else:
-        return render_template('new.html')
-
-@app.route('/ajax')
-def main1():
-    results = {}
-    return render_template('index.html', results = sheet3)
-
-@app.route('/user')
-def user():
-    return render_template('user.html')
-
-@app.route('/new')
-def new():
-    return render_template('new.html')
-
-@app.route('/old')
-def old():
-    return render_template('old.html')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__=='__main__':
+    app.run()
