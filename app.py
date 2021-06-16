4c18d82ab16bac36037bb5d02877f14cb47cd54b
brightleeee <84517690+brightleeee@users.noreply.github.com>
2021-06-16 16:37:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,46 +1,71 @@
 from typing import Any
 
-from flask import Flask, request, session,g,redirect,url_for, abort, flash, render_template, jsonify, json
+
+from flask import (
+    Flask,
+    request,
+    session,
+    g,
+    redirect,
+    url_for,
+    abort,
+    flash,
+    render_template,
+    jsonify,
+    json,
+)
 import pandas as pd
 import sqlite3
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def yeon():
     return render_template("login_test.html")
-@app.route('/index.html')
+
+
+@app.route("/index.html")
 def yeon1():
     return render_template("index.html")
-@app.route('/password.html')
+
+
+@app.route("/password.html")
 def yeon2():
     return render_template("password.html")
-@app.route('/test.html')
+
+
+@app.route("/test.html")
 def yeon3():
     return render_template("test.html")
 
-@app.route('/test3.html')
+
+@app.route("/test3.html")
 def yeon4():
     return render_template("test3.html")
 
-@app.route('/login.html')
+
+@app.route("/login.html")
 def yeon5():
     return render_template("login.html")
 
-@app.route('/test', methods=['POST'])
+
+@app.route("/test", methods=["POST"])
 def languages():
-    value1 = request.form['itemid']
-    value2 = request.form['itemid2']
+    value1 = request.form["itemid1"]
+    value2 = request.form["itemid2"]
+    value3 = request.form["itemid3"]
+    value4 = request.form["itemid4"]
+    value5 = request.form["itemid5"]
 
     print(value1)
     print(value2)
-    # value1 = "상가"
     print("데이터 넘어왔나?")
 
-    conn = sqlite3.connect('trest.db')
+    conn = sqlite3.connect("trest.db")
     curs = conn.cursor()
-
-    curs.execute('SELECT * FROM test1 where 용도=? and 주소지=?', (value1,value2)) #test1에 있는 값을 조건 없이 불러옴 or조건으로 입력값이 있는 것만 조회함
+    curs.execute('SELECT * FROM test1 where 1=1 and (?="" OR 면적=?) and (?="주거용 / 상업용" OR 용도=?)',
+                 (value1,value1,value4,value4))  # test1에 있는 값을 조건 없이 불러옴 or조건으로 입력값이 있는 것만 조회함
 
     # row_headers = [x[0] for x in curs.description]
     rows = curs.fetchall()
@@ -48,5 +73,6 @@ def languages():
     print("test2")
     return jsonify(rows)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     app.run()
