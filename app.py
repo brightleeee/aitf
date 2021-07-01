df760425330e3e9208cac1c98e076da1563a79d9
brightleeee <84517690+brightleeee@users.noreply.github.com>
2021-07-01 10:30:57 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -57,22 +57,46 @@ def languages():
     value3 = request.form["itemid3"]
     value4 = request.form["itemid4"]
     value5 = request.form["itemid5"]
+    value6 = request.form["itemid6"]
 
     print(value1)
-    print(value2)
+    print(value4)
     print("데이터 넘어왔나?")
 
     conn = sqlite3.connect("trest.db")
     curs = conn.cursor()
-    curs.execute('SELECT * FROM test1 where 1=1 and (?="" OR 면적=?) and (?="주거용 / 상업용" OR 용도=?)',
-                 (value1,value1,value4,value4))  # test1에 있는 값을 조건 없이 불러옴 or조건으로 입력값이 있는 것만 조회함
+    curs.execute('SELECT * FROM test2 where 1=1 and (?="" OR 세대수=?) and (?="선택하세요" OR 지역=?) and (?="선택하세요" OR 건물유형=?)',
+                 (value6, value6, value4, value4, value5, value5))  # test1에 있는 값을 조건 없이 불러옴 or조건으로 입력값이 있는 것만 조회함
 
     # row_headers = [x[0] for x in curs.description]
     rows = curs.fetchall()
-    print(rows)
+    # print(rows)
     print("test2")
     return jsonify(rows)
 
+@app.route("/test2", methods=["POST"])
+def languages2():
+    value1 = request.form["itemid1"]
+    value2 = request.form["itemid2"]
+    value3 = request.form["itemid3"]
+    value4 = request.form["itemid4"]
+    value5 = request.form["itemid5"]
+    value6 = request.form["itemid6"]
+
+    print(value1)
+    print(value4)
+    print("데이터 넘어왔나?")
+
+    conn = sqlite3.connect("trest.db")
+    curs = conn.cursor()
+    curs.execute('SELECT 연면적,용량 FROM test2 where 1=1 and (?="" OR 세대수=?) and (?="선택하세요" OR 지역=?) and (?="선택하세요" OR 건물유형=?)',
+                 (value6, value6, value4, value4, value5, value5))  # test1에 있는 값을 조건 없이 불러옴 or조건으로 입력값이 있는 것만 조회함
+
+    # row_headers = [x[0] for x in curs.description]
+    rows = curs.fetchall()
+    # print(rows)
+    print("test2")
+    return jsonify(rows)
 
 if __name__ == "__main__":
     app.run()
